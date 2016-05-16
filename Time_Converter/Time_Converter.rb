239161df55086f32bc7a2dabd7c9ec9e0870726e
srMarquinho <sr.marquinho@gmail.com>
2016-05-16 15:27:40 +0100

    update file

--- Time_Converter/Time_Converter.rb
+++ Time_Converter/Time_Converter.rb
@@ -1,17 +1,3 @@
-# DESCRIPTION:
-
-# Given a time in a time format class, return it without year, month and day.
-
-# It should return a string including milliseconds with 3 decimals.
-
-# Example:
-
-Time.new(2016, 2, 8, 16, 42, 59)
-#Should return: 
-"16:42:59,000"
-
-#------------------------------
-
 # TEST CASES
 
 Test.describe("Basic tests") do
